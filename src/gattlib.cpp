a593e6fa5f1bfddeaebcfaf267c3c7137e6608cc
John Sullivan <jsgthb@kanargh.org.uk>
2020-10-18 16:29:41 +0100

    don't leak refcount when gatt_connect() fails

--- src/gattlib.cpp
+++ src/gattlib.cpp
@@ -290,6 +290,7 @@ GATTRequester::connect(
 
     if (_channel == NULL) {
         _state = STATE_DISCONNECTED;
+        decref();
 
         std::string msg(gerr->message);
         int ecode = gerr->code;
